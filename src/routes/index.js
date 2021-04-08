a3a96753e47ebf309cedd3672de780fd76eee883
lucaslrf <lucasrodrigues.lrf@gmail.com>
2021-04-07 22:07:54 -0300

    Initial project

---
+++ src/routes/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { BrowserRouter as Router, Redirect, Route } from "react-router-dom";
+import "./styles.css";
+
+const Routes = () => {
+    
+}
+
+export default Routes;
